47ff22e9c5d652a0535585bb77288274bc03b68d
Claudia Engelsman <cmjengelsman@gmail.com>
2018-06-09 16:00:27 +0200

    Change homepage

---
+++ resources/views/index.blade.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title>Home - Build it faster</title>
+
+  <!-- Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
+  <!-- Styles -->
+  <link href="{{ URL::asset('/css/index.css') }}" rel="stylesheet" type="text/css">
+  <style>
+
+  </style>
+</head>
+<body>
+  <div class="flex-center position-ref full-height">
+    <div class="content">
+      <div class="title m-b-md">
+        <h1>Build it faster project</h1>
+      </div>
+      <div class="px-5">
+        <h3>About this project</h3>
+        <p>
+          This is a project to build website with HTML and CSS faster by practise. Designs here are not at all by me but found through other websites. I just write the code for practice.
+        </p>
+        <p>
+          The footer of each website will contain some extra information on how long it took to make the design into the HTML and CSS. Footers will also contain a link to the original design where I found it.
+        </p>
+        <p>
+          Start at the beginning of this project and when you've seen the page continue to the next by clicking any of the navigational links except the ones in the footer. Enjoy!
+        </p>
+      </div>
+      <div class="links">
+        <a href="https://github.com/Claudia413">Github</a>
+        <a href="https://www.linkedin.com/in/claudia-engelsman/">LinkedIn</a>
+        <a href="https://twitter.com/Claudia_blonde">Twitter</a>
+        <a href="http://http://nomajnomad.com/">Blog</a>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
